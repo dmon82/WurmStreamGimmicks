740f66f29306abfbf6eceae6455d545057aceb08
Cutiehotbutts69 <cutie@hotbutts.69>
2019-01-27 16:29:13 +0100

    DateTime extension to compare only hours, minutes, and seconds.

---
+++ WurmStreamGimmicks/Extentions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WurmStreamGimmicks {
+    static class Extentions {
+        public static bool EqualsToTheSecond(this DateTime value, DateTime compare) {
+            return value.Hour.Equals(compare.Hour) &&
+                value.Minute.Equals(compare.Minute) &&
+                value.Second.Equals(compare.Second);
+        }
+    }
+}
