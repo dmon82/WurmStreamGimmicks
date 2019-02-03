20341c98b3860db877be2335e80b7f7e00f69e70
Cutiehotbutts69 <cutie@hotbutts.69>
2019-02-03 22:36:04 +0100

    WIP on skillgain gimmick.

---
+++ WurmStreamGimmicks/BaseGimmick.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WurmStreamGimmicks {
+    abstract class BaseGimmick {
+        public virtual string Name { get; set; }
+        public virtual LogType Logs { get; set; }
+        public virtual string Template { get; set; }
+        public virtual bool Collective { get; set; }
+        public virtual List<string> Players { get; set; }
+        public abstract string Help { get; }
+
+        public virtual string Pattern { get; set; }
+        public virtual bool Events { get; set; }
+        public virtual  bool Combat { get; set; }
+        public virtual bool Skills { get; set; }
+        public virtual string OutputFile { get; set; }
+        public virtual bool Enabled { get; set; }
+
+        public BaseGimmick(string name, LogType logType, string template, bool collective, string pattern, string outputFile, List<string> players) {
+            Name = name;
+            Logs = logType;
+            Template = template;
+            Collective = collective;
+            Pattern = pattern;
+            OutputFile = outputFile;
+            Players = players;
+            Enabled = false;
+
+            Events = (logType & LogType.Events) == LogType.Events;
+            Combat = (logType & LogType.Combat) == LogType.Combat;
+            Skills = (logType & LogType.Skills) == LogType.Skills;
+        }
+
+        public BaseGimmick(string name, LogType logType, string template, bool collective, string pattern, string outputFile, params string[] players)
+            : this(name, logType, template, collective, pattern, outputFile, players.ToList()) {
+        }
+
+        public BaseGimmick(MyReader reader) {
+            Deserialise(reader);
+        }
+
+        public virtual void Write() {
+            try {
+                System.IO.File.WriteAllText(OutputFile, Compile());
+            }
+            catch (Exception e) {
+                Core.Logger.Log(LogLevel.Severe, "{0} could not update its output file '{1}'.", Name, OutputFile);
+                Core.Logger.Log(LogLevel.Severe, e.ToString());
+            }
+        }
+
+        public abstract string Compile();
+
+        public virtual void Serialise(MyWriter writer) {
+            writer.Write((int)0); // File versioning.
+
+            writer.Write(Name);
+            writer.Write((int)Logs);
+            writer.Write(Template);
+            writer.Write(Collective);
+            writer.Write(Players.Count);
+            Players.ForEach(x => writer.Write(x));
+
+            writer.Write(Pattern);
+            writer.Write(OutputFile);
+            writer.Write(Enabled);
+        }
+
+        public virtual void Deserialise(MyReader reader) {
+            int version = reader.ReadInt();
+
+            Name = reader.ReadString();
+            Logs = (LogType)reader.ReadInt();
+            Template = reader.ReadString();
+            Collective = reader.ReadBoolean();
+            int count = reader.ReadInt();
+            if (count > 0) Players = new List<string>();
+            while (count-- > 0) Players.Add(reader.ReadString());
+            
+            Pattern = reader.ReadString();
+            OutputFile = reader.ReadString();
+            Enabled = reader.ReadBoolean();
+        }
+    }
+}
---
+++ WurmStreamGimmicks/Gimmicks/SkillGain/SkillGainGimmick.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WurmStreamGimmicks {
+    class SkillGainGimmick : BaseGimmick, IGimmick {
+        public static readonly string Tooltip = "idk yet";
+        public override string Help { get { return SkillGainGimmick.Tooltip; } }
+        private static Regex _Regex = new Regex(@"(\w[\w\s]+) increased by (\d+\.\d+) to (\d+\.\d+)");
+
+        /// <summary>
+        ///     Gets or sets the skill name monitored.
+        /// </summary>
+        public string SkillName { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the number of skill ticks.
+        /// </summary>
+        public int Ticks { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the total amount of skill gained.
+        /// </summary>
+        public double Gain { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the smallest skill tick/
+        /// </summary>
+        public double SmallestGain { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the biggest skill tick.
+        /// </summary>
+        public double LargestGain { get; set; }
+
+        public SkillGainGimmick() : base("", LogType.Skills, "", false, "", "", new List<string>()) { }
+        public override string Compile() {
+            return "override compile";
+        }
+
+        public void Watch(string line, Player player) {
+            Match match = _Regex.Match(line);
+            
+            if (!match.Success) return;
+        }
+
+        public void Serialise(MyWriter writer) {
+            base.Serialise(writer);
+            writer.Write((int)0); // File format vesioning.
+        }
+
+        public void Deserialise(MyReader reader) {
+            base.Deserialise(reader);
+            int version = reader.ReadInt();
+        }
+    }
+}
